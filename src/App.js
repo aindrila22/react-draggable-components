56f35326831520e74cab2295b3efd501ca910f42
aindrila22 <aindrilabhattacharjee72@gmail.com>
2021-10-28 09:54:25 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef } from "react";
+import "./App.css";
+import useDraggable from "./useDraggable";
+
+const DraggableCard = ({ children }) => {
+  const cardRef = useRef(null);
+  useDraggable(cardRef);
+
+  return (
+    <div className="card" ref={cardRef}>
+      {children}
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <DraggableCard>Card 1</DraggableCard>
+      <DraggableCard>Card 2</DraggableCard>
+      <DraggableCard>Card 3</DraggableCard>
+      <DraggableCard>Card 4</DraggableCard>
+      <DraggableCard>Card 5</DraggableCard>
+      <DraggableCard>Card 6</DraggableCard>
     </div>
   );
 }
